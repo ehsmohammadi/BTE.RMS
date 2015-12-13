a54d3214f4ca6643067aab6763c16b09f8465acd
ehsmohammadi <ehs.mohammadi@gmail.com>
2015-12-13 10:55:03 +0330

    web updated

--- BTE.RMS.Presentation.Web/App_Start/BundleConfig.cs
+++ BTE.RMS.Presentation.Web/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace BTE.RMS.Presentation.Web
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.css", "~/Content/dashboard.css"));
         }
     }
 }
