a0df93286fa10f137f8bdba2134074f0d52d755b
ehsmohammadi <ehs.mohammadi@gmail.com>
2015-12-20 15:41:39 +0330

    unload wpf project

--- BTE.RMS.Interface.WebApi.Host/Controllers/OveralObjectiveController.cs
+++ BTE.RMS.Interface.WebApi.Host/Controllers/OveralObjectiveController.cs
@@ -1,33 +1,33 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
-using BTE.RMS.Interface.Contract;
+//using BTE.RMS.Interface.Contract;
 
 namespace BTE.RMS.Interface.WebApi.Host.Controllers
 {
     public class OveralObjectiveController : ApiController
     {
 
-        CrudOveralObjective[] overalObjectives = new CrudOveralObjective[] 
-        { 
-            new CrudOveralObjective { Id = 1, Overview = "Tomato Soup", Title = "Groceries" }, 
-            new CrudOveralObjective { Id = 2, Overview = "Yo-yo", Title = "Toys" }, 
-            new CrudOveralObjective { Id = 3, Overview = "Hammer", Title = "Hardware" } 
-        };
+        //CrudOveralObjective[] overalObjectives = new CrudOveralObjective[] 
+        //{ 
+        //    new CrudOveralObjective { Id = 1, Overview = "Tomato Soup", Title = "Groceries" }, 
+        //    new CrudOveralObjective { Id = 2, Overview = "Yo-yo", Title = "Toys" }, 
+        //    new CrudOveralObjective { Id = 3, Overview = "Hammer", Title = "Hardware" } 
+        //};
 
-        public IEnumerable<CrudOveralObjective> GetAllProducts()
-        {
-            return overalObjectives;
-        }
+        //public IEnumerable<CrudOveralObjective> GetAllProducts()
+        //{
+        //    return overalObjectives;
+        //}
 
-        public IHttpActionResult GetProduct(int id)
-        {
-            var product = overalObjectives.FirstOrDefault((p) => p.Id == id);
-            if (product == null)
-            {
-                return NotFound();
-            }
-            return Ok(product);
-        }
+        //public IHttpActionResult GetProduct(int id)
+        //{
+        //    var product = overalObjectives.FirstOrDefault((p) => p.Id == id);
+        //    if (product == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(product);
+        //}
     }
 }
