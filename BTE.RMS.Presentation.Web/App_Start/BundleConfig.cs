6b15b17e6495e3d322135f9d8c7b4569b5539c9a
ehsmohammadi <ehs.mohammadi@gmail.com>
2015-12-30 16:01:14 +0330

    calender added

--- BTE.RMS.Presentation.Web/App_Start/BundleConfig.cs
+++ BTE.RMS.Presentation.Web/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace BTE.RMS.Presentation.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+        
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -16,6 +17,10 @@ namespace BTE.RMS.Presentation.Web
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js", "~/Scripts/jquery.timepicker.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Calendar").Include(
+                    "~/Scripts/Calendar/jalali.js", "~/Scripts/Calendar/calendar.js", "~/Scripts/Calendar/calendar-setup.js", "~/Scripts/Calendar/calendar-fa.js"));
+
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -31,6 +36,9 @@ namespace BTE.RMS.Presentation.Web
 
             bundles.Add(new StyleBundle("~/Content/jqueryui")
                .Include("~/Content/themes/base/all.css", "~/Content/jquery.timepicker.css"));
+
+            bundles.Add(new StyleBundle("~/Content/Calendar")
+               .Include("~/Content/Calendar/calendar-blue.css"));
         }
     }
 }
