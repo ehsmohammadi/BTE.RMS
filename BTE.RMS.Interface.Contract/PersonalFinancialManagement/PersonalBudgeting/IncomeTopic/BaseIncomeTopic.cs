21517bf3b36684b35ef7fb005b18cabfedf8c4ba
amirhosseinamini <amirhamini75@gmail.com>
2016-01-24 19:47:08 +0330

    Regenerate Models

---
+++ BTE.RMS.Interface.Contract/PersonalFinancialManagement/PersonalBudgeting/IncomeTopic/BaseIncomeTopic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTE.Presentation;
+
+
+namespace BTE.RMS.Interface.Contract
+{
+    public class BaseIncomeTopic:ViewModelBase
+    {
+        private long id;
+
+        public long Id
+        {
+            get { return id; }
+            set { this.SetField(p => p.Id, ref id, value); }
+        }
+        private string title;
+
+        public string Title
+        {
+            get { return title; }
+            set { this.SetField(p => p.Title, ref title, value); }
+        }
+        private long monthlyIncome;
+
+        public long MonthlyIncome
+        {
+            get { return monthlyIncome; }
+            set { this.SetField(p => p.MonthlyIncome, ref monthlyIncome, value); }
+        }
+    }
+}
---
+++ BTE.RMS.Interface.Contract/PersonalFinancialManagement/PersonalBudgeting/IncomeTopic/SummeryIncomeTopic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BTE.Presentation;
+
+namespace BTE.RMS.Interface.Contract
+{
+    public class SummeryIncomeTopic : BaseIncomeTopic
+    {
+        private long yearlyIncome;
+
+        public long YearlyIncome
+        {
+            get { return yearlyIncome; }
+            set { this.SetField(p => p.YearlyIncome, ref yearlyIncome, value); }
+        }
+    }
+}
