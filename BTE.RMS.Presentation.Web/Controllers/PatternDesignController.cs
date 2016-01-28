2bb0dad77cebc418c29b10be535872bf51c8e3d0
amirhosseinamini <amirhamini75@gmail.com>
2016-01-28 07:53:22 +0330

    PatternDesign Controller

---
+++ BTE.RMS.Presentation.Web/Controllers/PatternDesignController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTE.RMS.Presentation.Web.Controllers
+{
+    public class PatternDesignController : Controller
+    {
+        //
+        // GET: /PatternDesign/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+        public ActionResult SecondaryObjectiveList()
+        {
+            return View();
+        }
+
+        public ActionResult CreateSecondaryObjective()
+        {
+            return View();
+        }
+        public ActionResult PersonalPlanning()
+        {
+            return View();
+        }
+
+        public ActionResult ProgramProgress()
+        {
+            return View();
+        }
+	}
+}
