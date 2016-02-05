86e03c0d7516d28f18d83483e4e09d25ac2db507
amirhosseinamini <amirhamini75@gmail.com>
2016-02-05 19:41:08 +0330

    Create Views

---
+++ BTE.RMS.Presentation.Web/Controllers/FinancPayReceiptController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTE.RMS.Presentation.Web.Controllers
+{
+    public class FinancPayReceiptController : Controller
+    {
+        //
+        // GET: /FinancPayReceipt/
+        public ActionResult FinancPayReceiptList()
+        {
+            return View();
+        }
+
+        public ActionResult CreateFinancReceipt()
+        {
+            return View();
+        }
+        public ActionResult CreateFinancPay()
+        {
+            return View();
+        }
+        public ActionResult EditFinancReceipt()
+        {
+            return View();
+        }
+        public ActionResult EditFinancPay()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteFinancReceipt()
+        {
+            return View();
+        }
+        public ActionResult DeleteFinancPay()
+        {
+            return View();
+        }
+	}
+}
---
+++ BTE.RMS.Presentation.Web/Controllers/PersonalBudgetingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BTE.RMS.Presentation.Web.Controllers
+{
+    public class PersonalBudgetingController : Controller
+    {
+        //
+        // GET: /PersonalBudgeting/
+        public ActionResult PrsBudgetingList()
+        {
+            return View();
+        }
+
+        public ActionResult CreatePrsBudgeting()
+        {
+            return View();
+        }
+
+        public ActionResult EditPrsBudgeting()
+        {
+            return View();
+        }
+
+        public ActionResult DeletePrsBudgeting()
+        {
+            return View();
+        }
+	}
+}
