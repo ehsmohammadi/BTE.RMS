2792f6f6ef178b7dca0562bf8a812186de8fd953
ehsmohammadi <ehs.mohammadi@gmail.com>
2016-02-17 23:42:15 +0330

    model updated

---
+++ BTE.RMS.Model/ISyncable.cs
@@ -0,0 +1,23 @@
+﻿namespace BTE.RMS.Model
+{
+    public class  Synceable
+    {
+        bool SyncedWithIphone { get; set; }
+        bool SyncedWithWeb { get; set; }
+        bool SyncedWithDesktop { get; set; }
+        bool SyncedWithAndriod { get; set; }
+
+        public Synceable()
+        {
+  
+        }
+
+        public void SyncWithIphone()
+        {
+            SyncedWithIphone = true;
+        }
+
+    }
+
+
+}
---
+++ BTE.RMS.Model/Task/Task.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BTE.RMS.Model.Task
+{
+    public abstract class Task
+    {
+
+        #region Properties & Backfields
+
+        private long id;
+        public long Id { get { return id; } }
+
+        private string title;
+        public string Title { get; set; }
+
+        private int workProgressPercent;
+        public int WorkProgressPercent { get { return workProgressPercent; } }
+
+        private DateTime startDate;
+        public DateTime StartDate { get { return startDate; } }
+
+        private DateTime startTime;
+        public DateTime StartTime { get { return startTime; }}
+
+        private DateTime endTime;
+        public DateTime EndTime { get { return endTime; }}
+
+        private bool synced;
+
+        #endregion
+
+        public Task(long id,PlatformTypeEnum platformType, string title, int workProgressPercent, DateTime startDate, DateTime startTime, DateTime endTime)
+        {
+            
+        }
+
+    }
+}
