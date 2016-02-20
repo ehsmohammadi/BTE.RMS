a66de936ae4646b11b57506d6a814aeeac8ddca9
ehsmohammadi <ehs.mohammadi@gmail.com>
2016-02-20 13:04:38 +0330

    task controller created

---
+++ BTE.RMS.Interface.WebApi.Host/Bootstrapper.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Web.ModelBinding;
+using System.Web.Mvc;
+using BTE.Core;
+using BTE.Presentation.Web;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+using ModelBinders = System.Web.Mvc.ModelBinders;
+
+namespace BTE.RMS.Presentation.Web
+{
+    public class Bootstrapper : IBootStrapper
+    {
+        public void Execute()
+        {
+            var container = new WindsorContainer().Install(FromAssembly.This());
+            //container.Kernel.ComponentModelBuilder.RemoveContributor(
+            //    container.Kernel.ComponentModelBuilder.Contributors.OfType<PropertiesDependenciesModelInspector>().Single());
+            //container.Kernel.ReleasePolicy = new NoTrackingReleasePolicy();
+
+            var locator = new WindsorServiceLocator(container);
+            ServiceLocator.SetLocatorProvider(() => locator);
+
+            var controllerFactory = new WindsorControllerFactory(container.Kernel);
+            ControllerBuilder.Current.SetControllerFactory(controllerFactory);
+            ModelBinders.Binders.DefaultBinder = new CastleWindsorModelBiner();
+        }
+    }
+
+
+    
+}
---
+++ BTE.RMS.Interface.WebApi.Host/Controllers/TasksController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using BTE.RMS.Interface.Contract.TaskItem;
+
+//using BTE.RMS.Interface.Contract;
+
+namespace BTE.RMS.Interface.WebApi.Host.Controllers
+{
+    public class TasksController : ApiController
+    {
+
+        CrudTaskItem[] overalObjectives = new CrudTaskItem[] 
+        { 
+            new CrudTaskItem { Id = 1, Title = "Groceries" }, 
+            new CrudTaskItem { Id = 2,  Title = "Toys" }, 
+            new CrudTaskItem { Id = 3, Title = "Hardware" } 
+        };
+
+        public IEnumerable<CrudTaskItem> GetAllProducts()
+        {
+            return overalObjectives;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = overalObjectives.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
