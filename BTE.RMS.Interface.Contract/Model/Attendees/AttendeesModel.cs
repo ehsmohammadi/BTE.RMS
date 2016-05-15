bb5612dd4cd6f828fd3de9f4d528eda110613da6
mjamein <jamei.mohammad@yahoo.com>
2016-05-14 21:33:28 -0700

    Meeting Model created

---
+++ BTE.RMS.Interface.Contract/Model/Attendees/AttendeesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTE.RMS.Interface.Contract.Model.Attendees
+{
+    public class AttendeesModel
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ BTE.RMS.Interface.Contract/Model/Meeting/MeetingModel.cs
@@ -0,0 +1,22 @@
+﻿using BTE.RMS.Interface.Contract.Model.Attendees;
+using BTE.RMS.Interface.Contract.Model.Location;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTE.RMS.Interface.Contract.Model.Meeting
+{
+    public class MeetingModel
+    {
+        public long Id { get; set; }
+        public string Subject { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Description { get; set; }
+        public LocationModel Location { get; set; }
+        public List<AttendeesModel> Attendees { get; set; }
+        public TimeSpan Duration { get; set; }
+
+    }
+}
