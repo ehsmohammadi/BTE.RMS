1a3561ddeaa31a9ea44b7c92efac442e4ab59c82
mjamein <jamei.mohammad@yahoo.com>
2016-05-16 23:23:00 -0700

    Remove unused using in Meeting Controllers

--- BTE.RMS.Presentation.Web/Controllers/MeetingsController.cs
+++ BTE.RMS.Presentation.Web/Controllers/MeetingsController.cs
@@ -1,9 +1,6 @@
 ﻿using BTE.RMS.Interface.Contract.Model.Meeting;
 using BTE.RMS.Interface.Contract.Facade;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using BTE.RMS.Presentation.Web.ViewModel.Meeting;
 
