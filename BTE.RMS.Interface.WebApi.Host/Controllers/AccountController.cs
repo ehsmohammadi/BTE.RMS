3f8e4609c6f521769c497bd0f066914fdaf9b434
ehsmohammadi <ehs.mohammadi@gmail.com>
2016-05-19 14:12:35 +0430

    Authentication updated

---
+++ BTE.RMS.Interface.WebApi.Host/Controllers/AccountController.cs
@@ -0,0 +1,79 @@
+﻿using System.Threading.Tasks;
+using System.Web.Http;
+using BTE.RMS.Interface.Contract.Model;
+using Microsoft.AspNet.Identity;
+
+namespace BTE.RMS.Interface.WebApi.Host.Controllers
+{
+    [RoutePrefix("api/Account")]
+    public class AccountController : ApiController
+    {
+        private AuthRepository repo = null;
+
+        public AccountController()
+        {
+            repo = new AuthRepository();
+        }
+
+        // POST api/Account/Register
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IHttpActionResult> Register(UserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            IdentityResult result = await repo.RegisterUser(userModel);
+
+            IHttpActionResult errorResult = GetErrorResult(result);
+
+            if (errorResult != null)
+            {
+                return errorResult;
+            }
+
+            return Ok();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                repo.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private IHttpActionResult GetErrorResult(IdentityResult result)
+        {
+            if (result == null)
+            {
+                return InternalServerError();
+            }
+
+            if (!result.Succeeded)
+            {
+                if (result.Errors != null)
+                {
+                    foreach (string error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error);
+                    }
+                }
+
+                if (ModelState.IsValid)
+                {
+                    // No ModelState errors are available to send, so just return an empty BadRequest.
+                    return BadRequest();
+                }
+
+                return BadRequest(ModelState);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ BTE.RMS.Interface.WebApi.Host/Security/AuthContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace BTE.RMS.Interface.WebApi.Host
+{
+
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
