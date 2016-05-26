28bbb7ec28db6e0365313061c8e5b06f88aafb35
seifm26 <mrseif26@gmail.com>
2016-05-26 17:28:23 +0430

    showmetting and edit

--- BTE.RMS.Presentation.Web/Controllers/MeetingsController.cs
+++ BTE.RMS.Presentation.Web/Controllers/MeetingsController.cs
@@ -23,6 +23,93 @@ namespace BTE.RMS.Presentation.Web.Controllers
             return View("MeetingList", viewModel);
         }
 
+        public ActionResult ShowMeeting()
+        {
+            List<MeetingShowViewModel> model = new List<MeetingShowViewModel>();
+            MeetingShowViewModel m1 = new MeetingShowViewModel()
+            {
+                Id = 1,
+                Subject = "موضوع",
+                StartTimeHour=10,
+                StartTimeminute=30,
+                EndTime="12:00",
+                Duration=90
+            };
+            MeetingShowViewModel m2 = new MeetingShowViewModel()
+            {
+                Id = 1,
+                Subject = "موضوع",
+                StartTimeHour = 13,
+                StartTimeminute=00,
+                EndTime = "16:20",
+                Duration = 200
+            };
+            MeetingShowViewModel m3 = new MeetingShowViewModel()
+            {
+                Id = 1,
+                Subject = "موضوع",
+                StartTimeHour = 00,
+                StartTimeminute = 00,
+                EndTime = "04:00",
+                Duration = 240
+            };
+            MeetingShowViewModel m4 = new MeetingShowViewModel()
+            {
+                Id = 1,
+                Subject = "موضوع",
+                StartTimeHour = 05,
+                StartTimeminute = 23,
+                EndTime = "08:23",
+                Duration = 180
+            };
+            MeetingShowViewModel m5 = new MeetingShowViewModel()
+            {
+                Id = 1,
+                Subject = "موضوع",
+                StartTimeHour = 10,
+                StartTimeminute = 00,
+                EndTime = "11:55",
+                Duration = 115
+            };
+            MeetingShowViewModel m6 = new MeetingShowViewModel()
+            {
+                Id = 1,
+                Subject = "موضوع",
+                StartTimeHour = 12,
+                StartTimeminute = 00,
+                EndTime = "14:20",
+                Duration = 140
+            };
+            MeetingShowViewModel m7 = new MeetingShowViewModel()
+            {
+                Id = 1,
+                Subject = "موضوع",
+                StartTimeHour = 17,
+                StartTimeminute = 30,
+                EndTime = "19:30",
+                Duration = 120
+            };
+            MeetingShowViewModel m8 = new MeetingShowViewModel()
+            {
+                Id = 1,
+                Subject = "موضوع",
+                StartTimeHour = 20,
+                StartTimeminute = 00,
+                EndTime = "24:00",
+                Duration = 240
+            };
+            model.Add(m1);
+            model.Add(m2);
+            model.Add(m3);
+            model.Add(m4);
+            model.Add(m5);
+            model.Add(m6);
+            model.Add(m7);
+            model.Add(m8);
+
+            return View(model);
+        }
+
         public ActionResult Create()
         {
 
@@ -46,7 +133,7 @@ namespace BTE.RMS.Presentation.Web.Controllers
 
         public ActionResult Edit(long id)
         {
-            throw new NotImplementedException();
+            return View();
         }
 
         public ActionResult Details(long id)
---
+++ BTE.RMS.Presentation.Web/ViewModel/Meeting/MeetingShowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BTE.RMS.Presentation.Web.ViewModel.Meeting
+{
+    public class MeetingShowViewModel
+    {
+        public long Id { get; set; }
+        public string Subject { get; set; }
+        public int StartTimeHour { get; set; }
+        public int StartTimeminute { get; set; }
+        public string EndTime { get; set; }
+        public int Duration { get; set; }
+
+
+    }
+}
