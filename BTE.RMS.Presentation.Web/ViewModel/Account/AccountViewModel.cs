dad1a26f0ae911abff4176f03ce345b8be69c886
seifm26 <mrseif26@gmail.com>
2016-05-28 20:25:54 +0430

    Account

---
+++ BTE.RMS.Presentation.Web/ViewModel/Account/AccountViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BTE.RMS.Presentation.Web.ViewModel.Account
+{
+    public class RegisterViewModel
+    {
+        public long Id { get; set; }
+
+
+        [Required(ErrorMessage = "نام کاربری را وارد نمایید")]
+        [Display(Name = "نام کاربری")]
+        public string Username { get; set; }
+
+
+        [Required(ErrorMessage="رمز عبور را وارد نمایید")]
+        [DataType(DataType.Password)]
+        [Display(Name = "رمز عبور")]
+        public string Password { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "تکرار رمز عبور")]
+        [Compare("Password", ErrorMessage = "با رمز عبور یکسان نمی باشد")]
+        public string ConfirmPassword { get; set; }
+    }
+}
