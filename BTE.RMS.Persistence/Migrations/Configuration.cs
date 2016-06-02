6a6ceed520cc763c0452b366bea5d47fc7b51ea2
ehsmohammadi <ehs.mohammadi@gmail.com>
2016-06-02 15:48:54 +0430

    Migration updated (issue #4)

--- BTE.RMS.Persistence/Migrations/Configuration.cs
+++ BTE.RMS.Persistence/Migrations/Configuration.cs
@@ -18,40 +18,7 @@ namespace BTE.RMS.Persistence.Migrations
 
         protected override void Seed(RMSContext context)
         {
-            var categories = new List<TaskCategory>
-            {
-                new TaskCategory("Work",Guid.NewGuid(),AppType.WebApp),
-                new TaskCategory("Friends",Guid.NewGuid(),AppType.WebApp)
-
-            };
-            foreach (var category in categories)
-                context.TaskCategories.AddOrUpdate(category);
-
-            var tasks = new List<Task>
-            {
-                new Task("نکته آموزشی", DateTime.Now, DateTime.Now, DateTime.Now,
-                    "بررسی اعماق زمین برای رسیدگی به موضوع", 33, categories.First(), AppType.WebApp, Guid.NewGuid()),
-                new Task("جلسه هفتگی با مدیران", DateTime.Now, DateTime.Now, DateTime.Now,
-                    "بررسی روند اقتصادی صنایع جهش محور", 33, categories.First(), AppType.WebApp, Guid.NewGuid())
-            };
-
-            foreach (var task in tasks)
-                context.Tasks.AddOrUpdate(task);
-
-
             base.Seed(context);
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
         }
     }
 }
