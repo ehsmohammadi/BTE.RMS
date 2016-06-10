b00f5934004fc0e2d9c3fc7932c46101cb11ffd2
ehsmohammadi <ehs.mohammadi@gmail.com>
2016-06-10 13:48:36 +0430

    updated last commit

---
+++ BTE.RMS.Services.Contract/Files/CreateFileCmd.cs
@@ -0,0 +1,9 @@
+﻿namespace BTE.RMS.Services.Contract
+{
+    public class CreateFileCmd
+    {
+        public string ContentType { get; set; }
+
+        public string Content { get; set; }
+    }
+}
